a527a5e4ba8adf6344cadece47f6e7eddc111467
mattebelle <matteobellettifc@gmail.com>
2025-02-26 17:39:39 +0100

    freeing up cache in llm_server

--- STE/llm_server.py
+++ STE/llm_server.py
@@ -10,11 +10,15 @@ HF_HOME = "/huggingface_cache"
 os.environ["HF_HOME"] = HF_HOME
 os.makedirs(HF_HOME, exist_ok=True)
 
+# device = torch.device("cuda:0")
+# # Set PyTorch to use only 90% of the GPU memory
+# torch.cuda.set_per_process_memory_fraction(0.9, device=device)
+
 #MODEL_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
 MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"
-print("DEBUGs (Server): Initializing tokenizer from MODEL_NAME.")
+print("DEBUGs (Server): Initializing tokenizer from MODEL_NAME.", flush=True)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, cache_dir=HF_HOME, max_length=4096)
-print(f"DEBUG (Server): Model and tokenizer loaded successfully. Cached at {HF_HOME}")
+print(f"DEBUG (Server): Model and tokenizer loaded successfully. Cached at {HF_HOME}", flush=True)
 
 tokenizer.add_special_tokens({"pad_token": "<PAD>"})
 # test = [
@@ -23,7 +27,7 @@ tokenizer.add_special_tokens({"pad_token": "<PAD>"})
 #   {"role": "user", "content": "I'd like to show off how chat templating works!"},
 # ]
 # print("CHAT_TEMPLATE APPLIED TO TEST: " + str(tokenizer.apply_chat_template(test, tokenize=False)))
-print("DEBUG (Server): Initializing model from MODEL_NAME.")
+print("DEBUG (Server): Initializing model from MODEL_NAME.", flush=True)
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
     torch_dtype=torch.float16,
@@ -45,14 +49,20 @@ async def generate_text(request: InferenceRequest):
     try:
         # Tokenize the provided prompt (which is already formatted by the client)
         #inputs = tokenizer(request.prompt, return_tensors="pt").to(model.device)
-        print("DENTRO GENERATEEEE------------------------------------------------------------------------------")
-        print("REQUEST PROMPT BEFORE TOKENIZATION: ", request.prompt)
-        print("REQUEST PROMPT AFTER TOKENIZATION: ", tokenizer.apply_chat_template(request.prompt, tokenize=False))
-        inputs = tokenizer(tokenizer.apply_chat_template(request.prompt, tokenize=False), return_tensors="pt").to("cuda")
+        print("DENTRO GENERATEEEE------------------------------------------------------------------------------", flush=True)
+        print("REQUEST PROMPT BEFORE TOKENIZATION: ", request.prompt, flush=True)
+        print("REQUEST PROMPT AFTER TOKENIZATION: ", tokenizer.apply_chat_template(request.prompt, tokenize=False), flush=True)
+        inputs = tokenizer(
+            tokenizer.apply_chat_template(request.prompt, tokenize=False),
+            return_tensors="pt",
+            truncation=True,
+            max_length=20000
+            ).to("cuda")
         token_ids = inputs.input_ids[0].tolist()
-        print("DEBUG (Server): Prompt length (tokens):", len(token_ids))
-        print("GENERANDO RISPOSTA GENERATEEEE------------------------------------------------------------------------------")
+        print("DEBUG (Server): Prompt length (tokens):", len(token_ids), flush=True)
+        print("GENERANDO RISPOSTA GENERATEEEE------------------------------------------------------------------------------", flush=True)
         # Generate output using the provided parameters
+        #with torch.no_grad():
         output_tokens = model.generate(
             **inputs,
             max_length=inputs.input_ids.shape[1] + request.max_tokens,
@@ -62,9 +72,13 @@ async def generate_text(request: InferenceRequest):
         )
         # Decode the full generated output
         generated_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
-        print("GENERATED TEXT= ", str(generated_text))
+        del inputs, output_tokens
+        torch.cuda.empty_cache()
+        print("GENERATED TEXT= ", str(generated_text), flush=True)
         return {"response": generated_text}
     except Exception as e:
+        # Ensure memory is cleared even on error
+        torch.cuda.empty_cache()
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
